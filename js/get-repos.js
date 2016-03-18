169a36325f27c54a7ed0fe4e14410037f12384d5
browneryan <browneryan87@gmail.com>
2016-03-18 11:34:39 -0700

    General cleanup

--- js/get-repos.js
+++ js/get-repos.js
@@ -1,12 +1,12 @@
 var apiKey = require('./../.env').apiKey;
+
 Search = function(){
 };
+
 exports.Search = Search;
 
 Search.prototype.getRepos = function(search){
   $.get("https://api.github.com/users/" + search + "/repos?access_token=" + apiKey).then(function(response){
-    console.log(search);
-
     $('.showUser').text("");
     $('.showUser').text("Your search for " + search + " returned: ");
     console.log(response);
