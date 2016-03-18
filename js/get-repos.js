8525ff91daba419dad05a4c6f51635095ef16109
browneryan <browneryan87@gmail.com>
2016-03-18 15:00:04 -0700

    Add ability to post repo name, description to get-repos.js

--- js/get-repos.js
+++ js/get-repos.js
@@ -6,12 +6,18 @@ Search = function(){
 exports.Search = Search;
 
 Search.prototype.getRepos = function(search){
+  $('.listUsers').empty();
   $.get("https://api.github.com/users/" + search + "/repos?access_token=" + apiKey).then(function(response){
-    $('.showUser').text("");
-    $('.showUser').text("Your search for " + search + " returned: ");
-    console.log(response);
+    $('.showUsers').append("Your search for " + search + " returned:");
+    $('.showUsers').show();
+    for (key of response) {
+      $('.listUsers').append("<h3>Name: " + key.name + "</h3>");
+      if (key.description) {
+      $('.listUsers').append("<h4>Description: " + key.description + "</h4>");
+      }
+    }
   }).fail(function(error){
-    $('.showUser').text(error.responseJSON.message);
+    $('.showUsers').text(error.responseJSON.message);
     console.log(error.responseJSON.message);
   });
 };
