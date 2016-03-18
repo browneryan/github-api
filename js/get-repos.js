06421d7a31e0bd804ec34f45c8e5f8028ab85c94
browneryan <browneryan87@gmail.com>
2016-03-18 11:29:36 -0700

    Add Search prototype getRepos to get-repos.js  that will return github info of inputted username

--- js/get-repos.js
+++ js/get-repos.js
@@ -1,7 +1,17 @@
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+var apiKey = require('./../.env').apiKey;
+Search = function(){
+};
+exports.Search = Search;
+
+Search.prototype.getRepos = function(search){
+  $.get("https://api.github.com/users/" + search + "/repos?access_token=" + apiKey).then(function(response){
+    console.log(search);
+
+    $('.showUser').text("");
+    $('.showUser').text("Your search for " + search + " returned: ");
     console.log(response);
   }).fail(function(error){
+    $('.showUser').text(error.responseJSON.message);
     console.log(error.responseJSON.message);
   });
 };
