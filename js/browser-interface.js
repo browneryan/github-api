20674af3ff86e202ea46f804c23adbac06b64ecb
browneryan <browneryan87@gmail.com>
2016-03-18 11:36:14 -0700

    Remove extraneous require, add form clear to end of function

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,12 +1,10 @@
-var getRepos = require('./../js/get-repos.js').getRepos;
 var Search = require("./../js/get-repos.js").Search;
 var newSearch = new Search();
 
 $(function() {
   $('#searchUsername').click(function() {
-    // $('#search').val("");
     var search = $('#search').val();
-    console.log(search);
     newSearch.getRepos(search);
+    $('#search').val("");
     });
   });
